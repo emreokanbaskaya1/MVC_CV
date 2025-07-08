0815acab0c163d51a78690b3119e9acd70f6a2d1
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-07-09 01:15:30 +0300

    Add project files.

---
+++ MVC_CV/Controllers/DefaultController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CV.Models.Entity;
+
+namespace MVC_CV.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimdas.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlers.ToList();
+            return PartialView(deneyimler);
+        }
+
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitims.ToList();
+            return PartialView(egitimler);
+        }
+
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerims.ToList();
+            return PartialView(yetenekler);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerims.ToList();
+            return PartialView(hobiler);
+        }
+
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifikalar = db.TblSertifikalarims.ToList();
+            return PartialView(sertifikalar);
+        }
+
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Iletisim(Tbliletisim t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString()); // Tarih bilgisini güncelle
+            db.Tbliletisims.Add(t);
+            db.SaveChanges();
+            ModelState.Clear(); // Formu temizle
+            return PartialView();
+        }
+    }
+}
