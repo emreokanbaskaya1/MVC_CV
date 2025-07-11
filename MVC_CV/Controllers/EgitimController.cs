0c7564178208a0242464f2dde9dc4534084d7cc4
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-07-11 12:59:51 +0300

    version3

---
+++ MVC_CV/Controllers/EgitimController.cs
@@ -0,0 +1,41 @@
+﻿using MVC_CV.Models.Entity;
+using MVC_CV.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CV.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEgitim> repo = new GenericRepository<TblEgitim>();
+        public ActionResult Index()
+        {
+            var egitim = repo.List(); ;
+            return View(egitim);
+        }
+
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEgitim p)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        
+
+    }
+}
